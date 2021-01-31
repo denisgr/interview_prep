237594df30da5e9b1d7428b51240a3498d96814b
Denis Gritsenko <degritse@microsoft.com>
2021-01-31 22:30:28 +0100

    6. ZigZag Conversion. 30 mins. Medium

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
+#include <sstream>
 using namespace std;
 
 class Solution {
 public:
-   string longestPalindrome(string s) {
-
-      for (size_t l = s.size(); l > 0; --l)
+   string convert(string s, int numRows) {
+      ostringstream oss;
+      for (size_t i = 0; i < numRows; ++i)
       {
-         for (size_t k = 0; k < s.size() - l; ++k)
+         int rowNumber = 0;
+         int increment = numRows != 1 ? 1 : 0;
+         size_t k = 0;
+         while (k < s.size())
          {
-            if (isPalindrome(k, l + k, s))
+            if (numRows != 1 && rowNumber == numRows - 1)
             {
-               return s.substr(k, l);
+               increment = -1;
             }
+            else if (numRows != 1 && rowNumber == 0)
+               increment = 1;
+            if (rowNumber == i)
+            {
+               oss << s[k];
+            }
+            rowNumber += increment;
+            ++k;
          }
       }
-      return "";
-   }
-
-   static bool isPalindrome(size_t l, size_t r, const string& s)
-   {
-      while(l < r)
-      {
-         if (s[l] != s[r])
-            return false;
-         l++; r--;
-      }
-      return true;
+      return oss.str();
    }
 };
 
