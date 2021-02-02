4290c804d850e33adcf8ddd967ef6711bdc60275
Denis Gritsenko <degritse@microsoft.com>
2021-02-02 22:46:53 +0100

    11. Container With Most Water. 20 mins. Medium

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -10,36 +10,26 @@ using namespace std;
 
 class Solution {
 public:
-   vector<int> twoSum(vector<int>& nums, int target) {
-      unordered_map<int, vector<int>> nums_with_indexes;
-      for (size_t i = 0; i < nums.size(); ++i)
+   int maxArea(vector<int>& height) {
+      int l = 0, h = static_cast<int>(height.size()) - 1;
+
+      int square = min(height[l], height[h]) * (h - l);
+
+      while (l < h)
       {
-         auto& v = nums_with_indexes[target - nums[i]];
-         v.push_back(i);
+         if (height[l] < height[h])
+         {
+            l++;
+         }
+         else
+            h--;
+         square = max(square, (h - l) * min(height[l], height[h]));
       }
 
-      for (int i = 0; i < nums.size(); ++i)
-      {
-         auto iter = nums_with_indexes.find(nums[i]);
-         if (iter != nums_with_indexes.end())
-         {
-            auto& v = iter->second;
-            for (int k = 0; k < v.size(); ++k)
-            {
-               if (v[k] != i)
-               {
-                  return { i,v[k] };
-               }
-            }
-         }
-      }
-      return {};
+      return square;
    }
 };
 
 int main()
 {
-   vector<int> v{ 3,2,4 };
-   Solution s;
-   s.twoSum(v, 6);
 }
